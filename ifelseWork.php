508544d80fb122eceb26a37ba43ca5998fd50aad
linlinboyz <a97549939@gmail.com>
2023-07-11 15:26:36 +0800

    first commit

---
+++ ifelseWork.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $num1 = $_POST["number1"];
+    $num2 = $_POST["number2"];
+    $num3 = $_POST["number3"];
+    $num4 = $_POST["number4"];
+    $max1 = 0;
+    $max2 = 0;
+    $mix1 = 0;
+    $mix2 = 0;
+
+    if($num1 > $num2){
+        $max1 = $num1;
+    } else if($num2 > $num1){
+        $max1 = $num2;
+    }
+
+    if($num3 > $num4){
+        $max2 = $num3;
+    } else if($num4 > $num3){
+        $max2 = $num4;
+    }
+
+    if($num1 > $num2){
+        $mix1 = $num2;
+    } else if($num2 > $num1){
+        $mix1 = $num1;
+    }
+
+    if($num3 > $num4){
+        $mix2 = $num4;
+    } else if($num4 > $num3){
+        $mix2 = $num3;
+    }
+    
+    if($max1 > $max2){
+        echo "最大的數為: $max1";
+    } else{
+        echo "最大的數為: $max2";
+    }
+
+    if($mix1 > $mix2){
+        echo "最小的數為: $mix2";
+    } else{
+        echo "最小的數為: $mix1";
+    }
+?>
+
+<form method = "post">
+    <label for="number1">數字1</label>
+    <input name="number1" type="text"/>
+    <br/>
+
+    <label for="number2">數字2</label>
+    <input name="number2" type="text"/>
+    <br/>
+
+    <label for="number3">數字3</label>
+    <input name="number3" type="text"/>
+    <br/>
+
+    <label for="number4">數字4</label>
+    <input name="number4" type="text"/>
+    <br/>
+
+    <input type="submit" name="submit"/>
+</form>
+</body>
+</html>
---
+++ randomWork.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    //指定隨機亂數
+    $firstArray = array(3,15,23,24,42,56,95);
+
+    echo $firstArray[rand(1,7)]."</br>";
+
+    //顯示兩個陣列的聯集
+    $a = array(1,3,6,7,9,10,11);
+    $b = array(2,7,3,9,8,11,10);
+    function merge($a,$b){
+        for($i = 0; $i < count($a); $i++){
+            for($j = 0; $j < count($b); $j++){
+                if($a[$i] == $b[$j]){
+                    echo $a[$i]." "; 
+                }
+            }
+        }
+    }
+    merge($a,$b);
+
+    ?>
+</body>
+</html>
